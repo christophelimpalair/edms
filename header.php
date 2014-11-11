bfebfc259d4907fc7334be23228c34eea1c93d88
Christophe G. Limpalair <christf24@hotmail.com>
2014-11-10 22:49:21 -0500

    Change CSS include
    Fix to relative URL issues

--- header.php
+++ header.php
@@ -15,10 +15,10 @@ require_once 'init.php';
 	<title><?php get_name(); ?></title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/edms/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="../css/custom.css" rel="stylesheet">
+    <link href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/edms/css/custom.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
