4d5c299ea108840a6a55f721bb1654a6d3dda3fd
Christophe Limpalair <christf24@hotmail.com>
2014-11-17 22:22:26 -0500

    Fix formatting

--- services/tabs/services.php
+++ services/tabs/services.php
@@ -4,23 +4,29 @@
 global $edmsdb;
 
 ?>
+<p>
+  <button type="button" class="btn btn-primary btn-lg menu" data-target="services_in_progress">In Progress</button>
+  <button type="button" class="btn btn-primary btn-lg menu" data-target="schedule_service">Schedule</button>
+  <button type="button" class="btn btn-primary btn-lg menu" data-target="search_service">Search</button>
+</p>
 
+<section id="services_in_progress" class="hidden">
  <h3>Services in Progress</h3>
   <div class="table-responsive">
     <table class="table table-striped">
      <thead>
      	<tr>
-            <th>Service ID</th>
-            <th>Technician Name</th>
-			<th>Advisor Name</th>
-			<th>Customer Name</th>
-			<th>Time Opened</th>
-			<th></th>
+          <th>Service ID</th>
+          <th>Technician Name</th>
+		  <th>Advisor Name</th>
+		  <th>Customer Name</th>
+		  <th>Time Opened</th>
+		  <th></th>
 		</tr>
       </thead>
 	  <tbody>
 
-	   </tbody>
+	  </tbody>
 <?php
 /**
  * Select
@@ -50,107 +56,145 @@ foreach( $query->fetchAll() as $currentServices )
 <?php
 }
 ?>
-	</tbody>
-</table>
-<h3>Schedule a Repair or Service</h3>
-			  <form role="schedule-service">
-				  <table class="table">
-					<tr>
-						<td><span class="glyphicon glyphicon-cog"></span> Advisor Name:</td>
-						<td>
-							<select class="form-control">
-								<option>Option 1</option>
-								<option>Option 2</option>
-								<option>Option 3</option>
-								<option>Option 4</option>
-								<option>Option 5</option>
-							</select></td>
-					</tr>
-					<tr>
-						<td><span class="glyphicon glyphicon-cog"></span> Technician Name:</td>
-						<td>
-							<select class="form-control">
-								<option>Option 1</option>
-								<option>Option 2</option>
-								<option>Option 3</option>
-								<option>Option 4</option>
-								<option>Option 5</option>
-							</select></td>
-					</tr>
-					<tr>
-						<td><span class="glyphicon glyphicon-plus"></span> Reason:</td>
-						<td>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Example: 5">
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td><span class="glyphicon glyphicon-usd"></span> VIN:</td>
-						<td>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Example: 56.99">
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td></td>
-						<td>
-							<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-ok"></span> Schedule</button>
-						</td>
-					</tr>
-				</table>
-			  </form>
-			
-		    <h3>Search Repairs & Services</h3>
-			 <h5>Only one of these fields is necessary</h5>
-			  <form role="parts-order">
-				  <table class="table">
-					<tr>
-						<td><span class="glyphicon glyphicon-cog"></span> Repair ID:</td>
-						<td>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Example: 1001">
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td><span class="glyphicon glyphicon-cog"></span> Customer Name:</td>
-						<td>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Example: Joel Miller">
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td><span class="glyphicon glyphicon-cog"></span> Technician Name:</td>
-						<td>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Example: David Lammee">
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td><span class="glyphicon glyphicon-cog"></span> Advisor Name:</td>
-						<td>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Example: David Nguyen">
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td><span class="glyphicon glyphicon-cog"></span> VIN Number:</td>
-						<td>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Example: LJCPCBLCX11000237">
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td></td>
-						<td>
-							<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span> Find</button>
-						</td>
-					</tr>
-				</table>
-			  </form>
+	  </tbody>
+	</table>
+</section>
+
+<section id="schedule_service" class="hidden">
+  <h3>Schedule a Repair or Service</h3>
+	<form role="schedule-service">
+	  <table class="table">
+		<tr>
+		  <td><span class="glyphicon glyphicon-cog"></span> Advisor Name:</td>
+		  <td>
+			<select class="form-control">
+			<?php
+			/**
+			 * Select
+			 */
+			$query = $edmsdb->prepare('SELECT * FROM employee WHERE department = "advisor"');
+			$query->execute();
+
+
+			/**
+			 * Loop through array of Parts table rows and echo out
+			 */
+			foreach( $query->fetchAll() as $advisor )
+			{
+			?>
+
+			  <option value="<?php echo $advisor["id"];?>"><?php echo $advisor["fname"] . " " . $advisor["lname"]; ?></option>
+			    
+			<?php
+			}
+			?>
+			</select>
+		  </td>
+		</tr>
+		<tr>
+		  <td><span class="glyphicon glyphicon-cog"></span> Technician Name:</td>
+		  <td>
+			<select class="form-control">
+		    <?php
+			/**
+			 * Select
+			 */
+			$query = $edmsdb->prepare('SELECT * FROM employee WHERE department = "technician"');
+			$query->execute();
+
+
+			/**
+			 * Loop through array of Parts table rows and echo out
+			 */
+			foreach( $query->fetchAll() as $technician )
+			{
+			?>
+
+			  <option value="<?php echo $technician["id"];?>"><?php echo $technician["fname"] . " " . $technician["lname"]; ?></option>
+			    
+			<?php
+			}
+			?>
+			</select>
+		  </td>
+		</tr>
+		<tr>
+		  <td><span class="glyphicon glyphicon-plus"></span> Reason: <small>(limit: 100 characters)</small></td>
+		  <td>
+			<div class="form-group">
+				<textarea class="form-control" rows="3" maxlength="100" placeholder="Enter reason(s) for service"></textarea>
+			</div>
+		  </td>
+		</tr>
+		<tr>
+		  <td><span class="glyphicon glyphicon-usd"></span> VIN:</td>
+		  <td>
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Example: LJCPCBLCX11000237">
+			</div>
+		  </td>
+		</tr>
+		<tr>
+		  <td></td>
+		  <td>
+			<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-ok"></span> Schedule</button>
+		  </td>
+		</tr>
+	 </table>
+	</form>
+</section>
+
+<section id="search_service" class="hidden">		
+    <h3>Search Repairs & Services</h3>
+	  <h5>Only one of these fields is necessary</h5>
+		<form role="parts-order">
+		  <table class="table">
+			<tr>
+			  <td><span class="glyphicon glyphicon-cog"></span> Repair ID:</td>
+			  <td>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Example: 1001">
+				</div>
+			  </td>
+			</tr>
+			<tr>
+			  <td><span class="glyphicon glyphicon-cog"></span> Customer Name:</td>
+			  <td>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Example: Joel Miller">
+				</div>
+			  </td>
+			</tr>
+			<tr>
+			  <td><span class="glyphicon glyphicon-cog"></span> Technician Name:</td>
+			  <td>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Example: David Lammee">
+				</div>
+			  </td>
+			</tr>
+			<tr>
+			  <td><span class="glyphicon glyphicon-cog"></span> Advisor Name:</td>
+			  <td>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Example: David Nguyen">
+				</div>
+			  </td>
+			</tr>
+			<tr>
+			  <td><span class="glyphicon glyphicon-cog"></span> VIN Number:</td>
+			  <td>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Example: LJCPCBLCX11000237">
+				</div>
+			  </td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>
+				<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span> Find</button>
+			  </td>
+			</tr>
+		</table>
+	</form>
+</section>
