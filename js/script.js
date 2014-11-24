b2d31b936755563d8ad20e74eb80891ed6e1c82e
Christophe Limpalair <christf24@hotmail.com>
2014-11-24 11:38:52 -0500

    Remove navigation for new page load structure
    No longer using jQuery to load content. Using PHP $_GET instead. This is
    much more scalable since I'm using AJAX to load/change content. Want
    updated content between screens

--- js/script.js
+++ js/script.js
@@ -4,30 +4,12 @@
  * Navigation
  */
 
-// Parts and Services Left Tabs
-$("ul.nav-sidebar li").on("click", function() {
-	$("ul.nav-sidebar li").not(this).removeClass("active");
-	$(this).addClass("active");
-	var target = "#" + $(this).data("target");
-	$(".col-sm-9").not(target).addClass("hidden");
-	$(target).removeClass("hidden");
-});
-
 // Parts and Services Report Images
 $(".placeholders .img-responsive").on("click", function() {
 	$(".placeholders .img-responsive").not(this).removeClass("active");
 	$(this).addClass("active");
 	var target = "#" + $(this).data("target");
 	$(".table-responsive.reports").not(target).addClass("hidden");
-	$(target).removeClass("hidden");
-});
-
-// Parts Menu Buttons
-$("button.menu").on("click", function() {
-	$("button.menu").not(this).removeClass("active");
-	$(this).addClass("active");
-	var target = "#" + $(this).data("target");
-	$("section").not(target).addClass("hidden");
 	$(target).removeClass("hidden");
 });
 
@@ -187,5 +169,7 @@ $("#search_parts_btn").on("click", function(e) {
 /**
  * # End Ajax Calls
  */
+
+//------------------------------------------------------
 
 }(jQuery));
